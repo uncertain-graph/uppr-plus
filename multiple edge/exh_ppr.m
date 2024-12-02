cf2bb397b7f096b7c8bf4d3c0848f5d8c0192aab
uppr <64377452+uncertain-graph@users.noreply.github.com>
2024-12-02 21:38:16 +0000

    Add files via upload

---
+++ multiple edge/exh_ppr.m
@@ -0,0 +1,63 @@
+function [ave_pprs, exhmem] = exh_ppr(a, c, qu_set, src, tar, kmax)
+   
+   num_qu_set = numel(qu_set);
+   num_src = numel(src);
+   %% enumerate all target set of possible worlds
+   nedges = numel(src);
+   for i = 1:nedges
+       target_sets{i} = getAllSubsets(tar{i});
+   end
+   
+   pw = CartesianProduct_multi(target_sets);
+   npw = size(pw,2)
+
+   % for i = 1:npw
+   %      disp(pw{i})
+   % end
+   qppr = 0;
+   n = size(a,1);    % # of nodes
+   memo = 0;
+   for qu = 1 : num_qu_set
+        nqu = numel(qu_set{qu});
+        qu_vec = sparse(qu_set{qu}', 1, 1/nqu, n, 1);
+        pwppr = 0;
+        for i = 1 : npw
+            a1 = a;
+            for p =1:size(src,2)
+                 %disp(pw{i})
+
+                for r = 1:size(pw{i}{p},2)
+                    if pw{i}{p}(r)
+                        a1(src(p),pw{i}{p}(r)) = 1;
+                    end
+                end 
+            end 
+    
+
+            % column norm
+            n = size(a1,1);    % # of nodes
+            d = full(sum(a1,2));      % in-degree vector
+            d_inv = 1./d;
+            d_inv(~isfinite(d_inv)) = 0;
+            w = a1' * spdiags(d_inv, 0, n, n);
+
+            [ppr, ppriterme] = iter_ppr(c, w, qu_vec, kmax);
+            pwppr = pwppr + ppr;
+            
+            memo = max(memo, ppriterme);
+        end
+        % compute average ppr over pws
+        pwppr = pwppr / npw;
+    
+        qppr = qppr + pwppr;
+        
+    end
+    ave_pprs = qppr / num_qu_set;
+    %ave_pprs = ave_pprs/norm(ave_pprs);
+    
+    
+    me = whos;
+    bytes = [me.bytes].';
+    exhmem = sum(bytes) + memo
+
+
---
+++ multiple edge/main.m
@@ -0,0 +1,105 @@
+
+clear;
+addpath("\BLin\");
+addpath("\metismex-master\build\mex\");
+addpath("\metismex-master\build\src\");
+%run("\metismex-master\METIS_startup.m");
+
+fpath =  'datasets\';
+
+%%%%%%%%%%%%%%%%%%%%% datasets %%%%%%%%%%%%%%%%%%%%%%%%%
+fname = 'cit-HepPh';
+fn = [fpath, fname, '.mat'];
+load(fn);
+a = Problem.A;
+%%%%%%%%%%%%%%%% hyperparameter %%%%%%%%%%%%%%%%%%%%%%%%%
+m = nnz(a);
+n = size(a,2);
+c = 0.8;
+kmax = 100;
+ncon = 1;
+nparts = 5;
+
+% %%%%%%%%%%%%%%%%% src, tar, qu %%%%%%%%%%%%%%%%%%%%%%%%%%
+% l = 6;
+% de = 4;
+% d = de-1;
+% 
+% src_tarfpath = [fpath,'src_tar\','ds_',fname,'_l',int2str(l),'_d',int2str(d),'.mat'];
+% qufpath = [fpath,'qu\','ds_',fname,'_qu','.mat'];
+% 
+% src = load(src_tarfpath).src';
+% 
+% tar =  load(src_tarfpath).tar;
+% 
+% qu_set = load(qufpath).qu;
+
+% savefpath = 'result\';
+% savefn = [savefpath, fname,'_l',int2str(l),'_d',int2str(d),'_res', '.mat'];
+% save(savefn, 'fname');
+
+a = sparse([ 
+     0     1     0     0     0     0     0     0
+     0     0     0     1     0     0     0     0
+     0     0     0     1     0     0     0     0
+     0     0     0     0     0     0     0     0
+     0     0     0     1     0     1     0     0
+     0     0     0     0     0     0     0     0
+     0     0     0     0     1     1     0     0
+     0     0     0     0     0     0     0     0
+    ]);
+src = [3,4,7];
+tar = {[5,6,8],[1],[4,8]};
+
+qu_set = {[2 5 7]};
+%%%%%%%%%%%%%%%%%%%%%%%%%% PPR Calculations %%%%%%%%%%%%%%
+[gt, exhmem]  = exh_ppr(a, src, tar, c, qu_set, 1);
+save(savefn, "gt")
+
+exht = tic;
+[res_exh, exhmem]  = exh_ppr(a, src, tar, c, qu_set, kmax);
+exh_time = toc(exht);   
+save(savefn, "res_exh","-append")
+
+exhApxt = tic;
+[res_exhApx, exhApxmem]= exhApx_ppr(a, c, qu_set, src, tar, ncon, nparts);
+exhApx_time = toc(exhApxt);
+save(savefn, "res_exhApx","-append")
+
+upprplust = tic;
+[res_upprplus, upprplusmem] = uppr_plus(a, c, qu_set, src, tar, fname);
+upprplus_time=toc(upprplust);
+save(savefn, "res_upprplus","-append") 
+
+upprt = tic;
+[res_uppr, upprmem] = uppr(a, c, qu_set, ncon, nparts, src, tar);
+uppr_time = toc(upprt);
+save(savefn, "res_uppr","-append") 
+ 
+collApxt = tic;
+[res_collApxppr, collApxmem] = collApx_ppr(a, c, qu_set, src, tar,ncon,nparts);
+collApx_time = toc(collApxt);
+save(savefn, "res_collApxppr","-append") 
+
+flatApxt = tic;
+[res_flatApx, flatApxmem]= flatApx_ppr(a, c, qu_set, src, tar,ncon, nparts);
+flatApx_time = toc(flatApxt);
+save(savefn, "res_flatApx","-append")
+
+
+
+%%%%%%%%%% save results %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+alg = {'upprplus';'uppr';'exh'; 'exhApx';'collApx';'flatApx'};
+%%%%%%%%%%% save time %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+time = [upprplus_time; uppr_time; exh_time; exhApx_time; collApx_time; flatApx_time];
+filename = 'excel_output\timeppr.xlsx';
+T1 = rows2vars(table(alg, time))
+writetable(T1, filename, 'Sheet',1, 'Range','D1');
+
+%%%%%%%%%%% save memory %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+memo = [upprplusmem; upprmem; exhmem; exhApxmem; collApxmem; flatApxmem];
+filename = 'excel_output\memoryppr.xlsx';
+T2 = rows2vars(table(alg, memo))
+writetable(T2, filename, 'Sheet',1, 'Range','D1');
+
+
