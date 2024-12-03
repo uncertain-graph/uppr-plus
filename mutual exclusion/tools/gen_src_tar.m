29fedd27f6868ade5dfc33d109c6dc03ff4550f5
uppr <64377452+uncertain-graph@users.noreply.github.com>
2024-12-03 13:54:55 +0000

    Add files via upload

---
+++ mutual exclusion/tools/gen_src_tar.m
@@ -0,0 +1,26 @@
+function  gen_src_tar(ds, l, d)
+
+    fpath =  '..\..\datasets\';
+    savepath = '..\src_tar\';
+  
+    fn = [fpath, ds, '.mat'];
+    load(fn);
+    a = Problem.A;
+
+   
+    n = length(a);
+
+    G = digraph(a);
+    D = outdegree(G);
+    [hd, idx] =  sort(D, "descend");
+    src = idx(1:l);
+
+    tar = cell(l,1);
+    for i = 1 : l
+        z = find(~a(src(i),:));
+        tar{i} = z(randperm(length(z), d));
+    end
+
+    savefn = [savepath,'ds_',ds,'_l',int2str(l),'_d',int2str(d), '.mat'];
+    disp(savefn)
+    save(savefn, 'src','tar');
