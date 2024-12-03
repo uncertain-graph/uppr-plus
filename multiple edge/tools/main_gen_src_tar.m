40a77db368041bf334a90f8897fcfef03a5c1069
uppr <64377452+uncertain-graph@users.noreply.github.com>
2024-12-03 12:45:34 +0000

    Update main_gen_src_tar.m

--- multiple edge/tools/main_gen_src_tar.m
+++ multiple edge/tools/main_gen_src_tar.m
@@ -1,19 +1,19 @@
-fpath = '/dcs/large/u5550119/datasets/';
+fpath = '..\datasets\';
 
 folderContents = dir(fpath);
 fileNames = {folderContents(~[folderContents.isdir]).name};
 ds_names = string(fileNames);
-ds_names = ["sk-2005.mat"];
+
 for i = 1: numel(ds_names)
     x = char(ds_names(i));
     dsname = x(1:end-4);
     
 
     for j =2:2:8
-        for k = 1:2:7
+        for k = 2:2:8
             gen_src_tar(dsname, j, k);
 
         end
     end
-   % gen_qu(dsname)
+    gen_qu(dsname)
  end
